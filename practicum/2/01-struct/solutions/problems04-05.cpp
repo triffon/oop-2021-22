d621363092816198378bfc9f5903fd40e5d1d027
Valentin Dimitrov <91693492+Dimitroffline@users.noreply.github.com>
2022-03-07 19:01:57 +0200

    Add files via upload

---
+++ practicum/2/01-struct/solutions/problems04-05.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+// 4. създайте структура distance, която има два члена - инчове и футове. напишете функция, 
+//    която я принтира във формата <футове>’<инчове>’’.
+
+// 5. напишете функция, която събира две дистанции. забележка: 1 фут = 12 инча.
+
+const int feet_to_inch = 12;
+
+struct distance
+{
+	float feet;
+	float inch;
+};
+
+void init(distance& dist)
+{
+	float inch;
+	float feet;
+
+	std::cin >> feet >> inch;
+
+	dist.feet = feet;
+
+	if (inch >= feet_to_inch)
+	{
+		dist.feet += floor(inch / feet_to_inch);
+		dist.inch = inch - floor(inch / feet_to_inch) * feet_to_inch;
+	}
+	else
+	{
+		dist.inch = inch;
+	}
+}
+
+distance sumdistance(distance& rhs, distance& lhs)
+{
+	distance result;
+
+	if (lhs.inch + rhs.inch >= feet_to_inch)
+	{
+		result.feet = rhs.feet + lhs.feet + 1;
+		result.inch = rhs.inch + lhs.inch - feet_to_inch;
+	}
+	else
+	{
+		result.feet = rhs.feet + lhs.feet;
+		result.inch = rhs.inch + lhs.inch;
+	}
+
+	return result;
+}
+
+void printdistance(distance& dist)
+{
+	std::cout << dist.feet << '\'' << dist.inch << '"';
+}
+
+int main()
+{
+	distance dist1, dist2;
+
+	init(dist1);
+	init(dist2);
+
+	distance result = sumdistance(dist1, dist2);
+
+	printdistance(result);
+
+	return 0;
+}
