09dd5f84cefd622f27bf9df6be842931ed748629
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-05-04 12:06:05 +0300

    Връщане на старата версия на битката

--- lectures/1/player/hero.cpp
+++ lectures/1/player/hero.cpp
@@ -17,3 +17,19 @@ void Hero::println(std::ostream& os) const {
     os << std::endl;
 }
 
+
+Hero* Hero::battle(Hero& hero1, Hero& hero2, unsigned prize) {
+    std::cout << "Заформя се епична битка между:" << std::endl;
+    hero1.println();
+    std::cout << "и" << std::endl;
+    hero2.println();
+    std::cout << "Кой ще спечели?" << std::endl;
+    Hero* winner = nullptr;
+    if (hero1.getLevel() > hero2.getLevel())
+        winner = &hero1;
+    else if (hero2.getLevel() > hero1.getLevel())
+        winner = &hero2;
+    if (winner != nullptr)
+        winner->addPoints(prize);
+    return winner;
+}
--- lectures/1/player/hero.hpp
+++ lectures/1/player/hero.hpp
@@ -18,26 +18,9 @@ public:
     void print(std::ostream& os = std::cout) const;
     void println(std::ostream& os = std::cout) const;
 
-    template <typename HeroType1, typename HeroType2>
-    static Hero* battle(HeroType1& hero1, HeroType2& hero2, unsigned prize = 1);
+    static Hero* battle(Hero& hero1, Hero& hero2, unsigned prize = 1);
 };
 
-template <typename HeroType1, typename HeroType2>
-Hero* Hero::battle(HeroType1& hero1, HeroType2& hero2, unsigned prize) {
-    std::cout << "Заформя се епична битка между:" << std::endl;
-    hero1.println();
-    std::cout << "и" << std::endl;
-    hero2.println();
-    std::cout << "Кой ще спечели?" << std::endl;
-    Hero* winner = nullptr;
-    if (hero1.getLevel() > hero2.getLevel())
-        winner = &hero1;
-    else if (hero2.getLevel() > hero1.getLevel())
-        winner = &hero2;
-    if (winner != nullptr)
-        winner->addPoints(prize);
-    return winner;
-}
 
 
 /* Искаме да реализираме наследник SuperHero на Hero, който:
