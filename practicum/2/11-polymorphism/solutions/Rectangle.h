93d9b3d59e1e6f982941502ce564cfbd048ed72a
Valentin Dimitrov <91693492+Dimitroffline@users.noreply.github.com>
2022-05-24 17:55:10 +0300

    Create Rectangle.h

---
+++ practicum/2/11-polymorphism/solutions/Rectangle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Shape.h"
+#include "Point2D.hpp"
+
+class Rectangle : public Shape
+{
+public:
+	Rectangle(Point2D bottom, Point2D upper);
+
+public:
+	virtual double getPerimeter() const override;
+	virtual Shape* clone() override;
+
+private:
+	Point2D bottomLeft;
+	Point2D upperRight;
+};
