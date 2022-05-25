9c152c82a2d254d3d83b8633cbfbde979465013b
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-05-25 10:49:37 +0300

    Реализация на факториел и степен чрез accumulate

--- lectures/1/highorder/main.cpp
+++ lectures/1/highorder/main.cpp
@@ -1,5 +1,39 @@
 #include <iostream>
 #include <cstring>
+#include <cassert>
+#include <cmath>
+
+using nextfun = int (*)(int);
+using mathfun = double (*)(double);
+using mathop  = double (*)(double, double);
+
+double accumulate(mathop op, double nv,
+                  double a, double b,
+                  mathfun f, nextfun next) {
+    double result = nv;
+    for(int i = a; i <= b; i = next(i))
+        result = op(result, f(i));
+    return result;
+}
+
+double multiply(double x, double y) { return x * y; }
+int plus1(int x) { return x + 1; }
+double id(double x) { return x; }
+
+unsigned fact(unsigned n) {
+    return accumulate(multiply, 1, 1, n, id, plus1);
+}
+
+// !!! template <double x>
+double gx;
+double constx(double i) { return gx; }
+
+double power(double x, long n) {
+    gx = x;
+    if (n < 0)
+        return 1 / power(x, -n);
+    return accumulate(multiply, 1, 1, n, constx, plus1);
+}
 
 void f() {
     std::cout << "f()\n";
@@ -9,7 +43,6 @@ void g() {
     std::cout << "g()\n";
 }
 
-int plus1(int x) { return x + 1; }
 
 void testFunctionPointers() {
     std::cout << (void*)f << std::endl;
@@ -35,8 +68,32 @@ void testFunctionPointers() {
         //(*fptr)();
 }
 
+void testFact() {
+    assert(fact(0) == 1);
+    assert(fact(1) == 1);
+    assert(fact(5) == 120);
+    assert(fact(7) == 5040);
+}
+
+void testPower() {
+    assert(power(5, 0) == 1);
+    assert(power(2, 5) == 32);
+    assert(power(3, 3) == 27);
+    assert(power(2, 10) == 1024);
+    assert(power(0, 5) == 0);
+    assert(power(1, 5) == 1);
+    assert(power(2, -2) == .25);
+    assert(fabs(power(3, -5) - 0.004115226) < 1E-6);
+}
+
+void testAccumulate() {
+    testFact();
+    testPower();
+}
+
 int main(int, char**) {
     std::cout << (long)main << std::endl;
     std::cout << (void*)strcpy << std::endl;
-    testFunctionPointers();
+    // testFunctionPointers();
+    testAccumulate();
 }
