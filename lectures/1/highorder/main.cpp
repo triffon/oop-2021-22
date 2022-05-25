bf34f5cd44e8ab39e3fce0c38cd992fbdf8e39f3
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2022-05-25 11:55:20 +0300

    Производна на функция

--- lectures/1/highorder/main.cpp
+++ lectures/1/highorder/main.cpp
@@ -17,11 +17,11 @@ double accumulate(mathop op, double nv,
 }
 
 double multiply(double x, double y) { return x * y; }
+double add(double x, double y) { return x + y; }
 int plus1(int x) { return x + 1; }
-double id(double x) { return x; }
 
 unsigned fact(unsigned n) {
-    return accumulate(multiply, 1, 1, n, id, plus1);
+    return accumulate(multiply, 1, 1, n, [](double x) { return x; }, plus1);
 }
 
 // !!! template <double x>
@@ -33,6 +33,29 @@ double power(double x, long n) {
     if (n < 0)
         return 1 / power(x, -n);
     return accumulate(multiply, 1, 1, n, constx, plus1);
+}
+
+double expterm(double i) {
+    return power(gx, i) / fact(i);
+}
+
+double myexp(double x, unsigned n) {
+    gx = x;
+    return accumulate(add, 0, 0, n, expterm, plus1);
+}
+
+/*
+mathfun gf;
+double derivative(double x) {
+    const double DX = 1E-6;
+    return (gf(x + DX) - gf(x)) / DX;
+}
+*/
+
+auto derive(auto f) {
+// !!!    gf = f;
+    const double DX = 1E-6;
+    return [f,DX](double x) { return (f(x + DX) - f(x)) / DX; };
 }
 
 void f() {
@@ -86,9 +109,35 @@ void testPower() {
     assert(fabs(power(3, -5) - 0.004115226) < 1E-6);
 }
 
+void testExp() {
+    const double PRECISION = 1E-3;
+    assert(fabs(myexp(0, 10) - 1) < PRECISION);
+    assert(fabs(myexp(1, 10) - exp(1)) < PRECISION);
+    std::clog.precision(6);
+    std::clog.setf(std::ios::fixed);
+    std::clog << myexp(2.3, 13) << ' ' << exp(2.3) << std::endl;
+    assert(fabs(myexp(2.3, 13) - exp(2.3)) < PRECISION);
+}
+
+void assertDoubleEqual(double x, double y, double precision = .1) {
+    std::clog << "Testing " << x << " = " << y << std::endl;
+    assert(fabs(x - y) < precision);
+}
+
+void testDerive() {
+    assertDoubleEqual(derive([](double x) { return x * x; })(5), 10);
+    assertDoubleEqual(derive(sin)(3), cos(3));
+    assertDoubleEqual(derive(exp)(5), exp(5));
+    assertDoubleEqual(derive(derive([](double x) { return x * x * x; }))(5), 30);
+    assertDoubleEqual(derive(derive(sin))(3), -sin(3));
+    assertDoubleEqual(derive(derive(exp))(5), exp(5));
+}
+
 void testAccumulate() {
     testFact();
     testPower();
+    testExp();
+    testDerive();
 }
 
 int main(int, char**) {
